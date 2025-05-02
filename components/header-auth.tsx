3b5ccc4810d46556964ac6d50f6e2071afb8d21d
evmaperry <evmaperry@gmail.com>
2025-05-02 17:47:32 -0400

    initial commit

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,9 +1,25 @@
-import { signOutAction } from "@/app/actions";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import Link from "next/link";
-import { Badge } from "./ui/badge";
-import { Button } from "./ui/button";
-import { createClient } from "@/utils/supabase/server";
+import { signOutAction } from '@/app/actions';
+import { hasEnvVars } from '@/utils/supabase/check-env-vars';
+import Link from 'next/link';
+import { Badge } from './ui/badge';
+import { Button } from './ui/button';
+import { createClient } from '@/utils/supabase/server';
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from '@/components/ui/menubar';
+import { DoorOpen, Eraser, LogOut, Menu, Pencil } from 'lucide-react';
 
 export default async function AuthButton() {
   const supabase = await createClient();
@@ -15,33 +31,33 @@ export default async function AuthButton() {
   if (!hasEnvVars) {
     return (
       <>
-        <div className="flex gap-4 items-center">
+        <div className='flex gap-4 items-center'>
           <div>
             <Badge
-              variant={"default"}
-              className="font-normal pointer-events-none"
+              variant={'default'}
+              className='font-normal pointer-events-none'
             >
               Please update .env.local file with anon key and url
             </Badge>
           </div>
-          <div className="flex gap-2">
+          <div className='flex gap-2'>
             <Button
               asChild
-              size="sm"
-              variant={"outline"}
+              size='sm'
+              variant={'outline'}
               disabled
-              className="opacity-75 cursor-none pointer-events-none"
+              className='opacity-75 cursor-none pointer-events-none'
             >
-              <Link href="/sign-in">Sign in</Link>
+              <Link href='/sign-in'>Sign in</Link>
             </Button>
             <Button
               asChild
-              size="sm"
-              variant={"default"}
+              size='sm'
+              variant={'default'}
               disabled
-              className="opacity-75 cursor-none pointer-events-none"
+              className='opacity-75 cursor-none pointer-events-none'
             >
-              <Link href="/sign-up">Sign up</Link>
+              <Link href='/sign-up'>Sign up</Link>
             </Button>
           </div>
         </div>
@@ -49,21 +65,51 @@ export default async function AuthButton() {
     );
   }
   return user ? (
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
-          Sign out
-        </Button>
-      </form>
+    <div className='flex items-center gap-4'>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>
+            <Menu size={18} />
+          </MenubarTrigger>
+          <MenubarContent className='mr-12 px-4'>
+            <MenubarItem>{user.email}</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>
+              <form
+                action={signOutAction}
+                className='flex flex-row items-center justify-between w-full'
+              >
+                <LogOut />
+                <div>Sign out</div>
+              </form>
+            </MenubarItem>
+            <MenubarItem>
+              <Link href={'/dashboard/reset-password'} className='flex flex-row items-center justify-between w-full'><Pencil />Change password</Link>
+            </MenubarItem>
+            {/* <MenubarItem disabled>New Incognito Window</MenubarItem>
+            <MenubarSub>
+              <MenubarSubTrigger>Share</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>Email link</MenubarItem>
+                <MenubarItem>Messages</MenubarItem>
+                <MenubarItem>Notes</MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSeparator />
+            <MenubarItem>
+              Print... <MenubarShortcut>⌘P</MenubarShortcut>
+            </MenubarItem> */}
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
     </div>
   ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
+    <div className='flex gap-2'>
+      <Button asChild size='sm' variant={'outline'}>
+        <Link href='/sign-in'>Sign in</Link>
       </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
+      <Button asChild size='sm' variant={'default'}>
+        <Link href='/sign-up'>Sign up</Link>
       </Button>
     </div>
   );
